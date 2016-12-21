ca67682e4ee6a650050a6911077b9043882b6b3b
Joseph Curtis <joecurt85@gmail.com>
2016-12-21 10:51:58 -0500

    create more HTML structure and CSS stylings

--- src/components/middle.js
+++ src/components/middle.js
@@ -5,6 +5,9 @@ class Middle extends React.Component {
     constructor(props) {
         super(props);
         this.rootURL = 'https://jsonplaceholder.typicode.com';
+        this.dataSet = [
+            { "assignee": "John Smith", "dateDue": "Dec 23rd, 2016", "risk": "High Risk", "value": "$5000", "entity": "Holland & Sherry Store" }
+        ];
     }
 
 
@@ -20,41 +23,56 @@ class Middle extends React.Component {
                         <section className="main-panel_heading"><span className="main-dashboard_span">Opportunities</span> { `${this.props.title}` }</section>
                         <section className="panel-body">
                            <section className="col-lg-8 col-md-8 col-sm-8">
-                                   <section className="col-lg-2">
-                                       Test
+                               <span className="dashboard-icon"><i className="fa fa-user" aria-hidden="true"></i></span>
+                               <section className="widget-user col-lg-2">
+                                   <div className="col-lg-12 text-center widget-user_title"><h6>assigned to:</h6></div>
+                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].assignee }</h6></div>
+                               </section>
+                               <span className="dashboard-icon"><i className="fa fa-calendar" aria-hidden="true"></i></span>
+                               <section className="widget-user col-lg-2">
+                                   <div className="col-lg-12 text-center widget-user_title"><h6>due date:</h6></div>
+                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].dateDue }</h6></div>
+                               </section>
+                               <span className="dashboard-icon"><i className="fa fa-exclamation-triangle" aria-hidden="true"></i></span>
+                               <section className="widget-user col-lg-2">
+                                   <div className="col-lg-12 text-center widget-user_title"><h6>risk factor:</h6></div>
+                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].risk }</h6></div>
+                               </section>
+                               <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"></i></span>
+                               <section className="widget-user col-lg-2">
+                                   <div className="col-lg-12 text-center widget-user_title"><h6>values:</h6></div>
+                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].value }</h6></div>
+                               </section>
+                               <section className="col-lg-12">
+                                   <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"></i></span>
+                                   <section className="widget-user col-lg-2">
+                                       <div className="col-lg-12 text-left widget-user_title"><h6>entity:</h6></div>
+                                       <div className="col-md-12 text-left widget-user_data"><h6>{ this.dataSet[0].entity }</h6></div>
                                    </section>
-                                   <section className="col-lg-2">
-                                       Test
-                                   </section>
-                                   <section className="col-lg-2">
-                                       Test
-                                   </section>
-                                   <section className="col-lg-2">
-                                       Test
-                                   </section>
-                                    <section className="col-lg-12">
-                                        Test
-                                    </section>
+                               </section>
                                     <section className="col-lg-12">
                                         Store <a href="#">Boston(130)</a> was found with High Damage Registration for High Risk Products in the last 7 days compare to previous 14 days and compare to North region bench mark activity.
                                     </section>
                                </section>
                             <section className="col-lg-4 col-md-4 col-sm-4">
                                 <section className="main-dashboard_panel">
-                                    <h1>Status</h1>
+                                    <section className="main-dashboard_title">Status</section>
                                 </section>
                                 <section className="main-dashboard_info">
                                     In Progress
                                 </section>
                             </section>
                             <section className="col-lg-8 col-md-8 col-sm-8">
-                                <h1>Test</h1>
+                                <ul>
+                                    <li>Caught Items(4)</li>
+                                    <li>Best Practices(3)</li>
+                                    <li>Tasks(3)</li>
+                                    <li>History</li>
+                                </ul>
                             </section>
                             <section className="col-lg-4 col-md-4 col-sm-4">
-                                <h1>Test</h1>
                             </section>
                         </section>
-                        <section style={{ color: 'blue'}} className="panel-footer">{ `${ 8 * 256 }` }</section>
                     </section>
                 </section>
             </section>
