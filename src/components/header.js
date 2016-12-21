3ed038b469d2ad55bd94f4680c754bb92d6b4106
Joseph Curtis <joecurt85@gmail.com>
2016-12-20 23:22:48 -0500

    make animation for search bar the way I thought looked nice

--- src/components/header.js
+++ src/components/header.js
@@ -8,19 +8,14 @@ class Header extends React.Component {
 
     render() {
         return (
-            <header style={{ background: this.props.background }} className="col-lg-12 col-md-12 col-sm-12">
-                <section className="header-logo col-lg-8 col-md-8 col-sm-6">
+            <header style={{ background: this.props.background }} className="col-lg-12 col-md-12 col-sm-12 header-main">
+                <section className="header-logo col-lg-6 col-md-6 col-sm-6">
                     <h1 style={{ color: (this.props.color) ? '#fff' : '#333' }} >{ this.props.name }</h1>
                 </section>
-                    <section className="header-toolBar col-lg-4 col-md-4 col-sm-4">
+                    <section className="header-toolBar col-lg-6 col-md-6 col-sm-6">
                         <ul className="header-toolBar_ul">
-                            <li>
-                                <input type="search" id="search" placeholder="What are you looking for?"></input>
-                            </li>
-                            <li>
-                                <i className="fa fa-search" aria-hidden="true">
-                                </i>
-                                <span className="header-toolBar_ul-search"> Search </span>
+                            <li className="header-toolBar_anim">
+                                <input type="search" placeholder="Search..."></input>
                             </li>
                             <li>
                                 <UserProfile/>
