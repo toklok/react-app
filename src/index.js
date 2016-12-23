51dba03d6066d7ad017332f2895951388dafe169
Joseph Curtis <joecurt85@gmail.com>
2016-12-22 21:25:50 -0500

    changed from postcss to sass, updated webpack.. found bug in webpack

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main'
 
+require("./css/main.scss");
+
 if (module.hot) {
     module.hot.accept()
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path'),
     webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
+    debug: true,
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
@@ -36,15 +38,17 @@ module.exports = {
                 loader: 'babel'
             },
             {
-                test: /\.css/,
-                loader: [
-                    'style-loader',
-                    'css-loader?importLoaders=1',
-                    'postcss-loader?sourceMap=inline'
-                ]
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract(
+                    'style',
+                    'css?sourceMap!sass?sourceMap'
+                )
             },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('main.css')
+    ]
 }
