50bf688f9a31faefc3e600a942f3dc4eda1acfd2
Joseph Curtis <joecurt85@gmail.com>
2016-12-26 21:30:16 -0500

    updated a few more things

--- src/components/middle.js
+++ src/components/middle.js
@@ -34,58 +34,83 @@ class Middle extends React.Component {
             <section className="col-lg-12 main-dashboard">
                 <section className="col-lg-12">
                     <section className="panel panel-default">
-                        <section className="main-panel_heading"><span className="main-dashboard_span">Opportunities</span> { `${this.props.title}` }</section>
+                        <section className="main-panel_heading"><span
+                            className="main-dashboard_span">Opportunities</span> { `${this.props.title}` }</section>
                         <section className="panel-body">
-                           <section className="col-lg-9">
-                               <span className="dashboard-icon"><i className="fa fa-user" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-3">
-                                   <div className="widget-user_title"><h4>assigned to:</h4></div>
-                                   <div className="widget-user_data"><h3>{ this.dataSet[0].assignee }</h3></div>
-                               </section>
-                               <span className="dashboard-icon"><i className="fa fa-calendar" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-3">
-                                   <div className="widget-user_title"><h4>due date:</h4></div>
-                                   <div className="widget-user_data"><h3>{ this.dataSet[0].dateDue }</h3></div>
-                               </section>
-                               <span className="dashboard-icon"><i className="fa fa-exclamation-triangle" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-3">
-                                   <div className="widget-user_title"><h4>risk factor:</h4></div>
-                                   <div className="widget-user_data"><h3>{ this.dataSet[0].risk }</h3></div>
-                               </section>
-                               <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-3">
-                                   <div className="widget-user_title"><h4>values:</h4></div>
-                                   <div className="widget-user_data"><h3>{ this.dataSet[0].value }</h3></div>
-                               </section>
-                               <section className="dashboard-entity">
-                                   <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"/></span>
-                                   <section className="widget-user col-lg-3">
-                                       <div className="widget-user_title"><h4>entity:</h4></div>
-                                       <div className="widget-user_data"><h3>{ this.dataSet[0].entity }</h3></div>
-                                   </section>
-                               </section>
-                                    <section className="dashboard-notifications_alert">
-                                        <p>Store <a href="#">Boston(130)</a> was found with High Damage Registration for High Risk Products in the last 7 days compare to previous 14 days and compare to North region bench mark activity.</p>
-                                    </section>
-                               </section>
-                            <section className="col-lg-3 dashboard-rightSide">
-                                <section className="main-dashboard_panel">
-                                    <section className="main-dashboard_title">
-                                        <h4>Status</h4>
+                            <section className="col-lg-9">
+                                <span className="dashboard-icon"><i className="fa fa-user" aria-hidden="true"/></span>
+                                <section className="widget-user col-lg-3">
+                                    <div className="widget-user_title"><h4>assigned to:</h4></div>
+                                    <div className="widget-user_data"><h3>{ this.dataSet[0].assignee }</h3></div>
+                                </section>
+                                <span className="dashboard-icon"><i className="fa fa-calendar" aria-hidden="true"/></span>
+                                <section className="widget-user col-lg-3">
+                                    <div className="widget-user_title"><h4>due date:</h4></div>
+                                    <div className="widget-user_data"><h3>{ this.dataSet[0].dateDue }</h3></div>
+                                </section>
+                                <span className="dashboard-icon"><i className="fa fa-exclamation-triangle" aria-hidden="true"/></span>
+                                <section className="widget-user col-lg-3">
+                                    <div className="widget-user_title"><h4>risk factor:</h4></div>
+                                    <div className="widget-user_data"><h3>{ this.dataSet[0].risk }</h3></div>
+                                </section>
+                                <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"/></span>
+                                <section className="widget-user col-lg-3">
+                                    <div className="widget-user_title"><h4>values:</h4></div>
+                                    <div className="widget-user_data"><h3>{ this.dataSet[0].value }</h3></div>
+                                </section>
+                                <section className="dashboard-entity">
+                                    <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"/></span>
+                                    <section className="widget-user col-lg-3">
+                                        <div className="widget-user_title"><h4>entity:</h4></div>
+                                        <div className="widget-user_data"><h3>{ this.dataSet[0].entity }</h3></div>
                                     </section>
                                 </section>
-                                <section className="main-dashboard_info">
-                                    <h1>In Progress</h1>
+                                <section className="dashboard-notifications_alert">
+                                    <p>Store <a href="#">Boston(130)</a> was found with High Damage Registration for
+                                        High Risk Products in the last 7 days compare to previous 14 days and compare to
+                                        North region bench mark activity.</p>
                                 </section>
                             </section>
-                            <section className="col-lg-3 dashboard-rightSide">
-                                <section className="main-dashboard_panel">
-                                    <section className="main-dashboard_title">
-                                        <h4>Status</h4>
+                            <section className="dashboard-rightSide_wrapper">
+                                <section className="col-lg-12 dashboard-rightSide_container">
+                                    <section className="main-dashboard_panel">
+                                        <section className="main-dashboard_title">
+                                            <h4>Status</h4>
+                                        </section>
+                                    </section>
+                                    <section className="main-dashboard_info">
+                                        <h1>In Progress</h1>
                                     </section>
                                 </section>
-                                <section className="main-dashboard_info">
-                                    <h1>In Progress</h1>
+                                <section className="col-lg-12 dashboard-rightSide_container">
+                                    <section className="main-dashboard_panel">
+                                        <section className="main-dashboard_title">
+                                            <h4>Status</h4>
+                                        </section>
+                                    </section>
+                                    <section className="main-dashboard_info">
+                                        <h1>In Progress</h1>
+                                    </section>
+                                </section>
+                                <section className="col-lg-12 dashboard-rightSide_container">
+                                    <section className="main-dashboard_panel-blue">
+                                        <section className="main-dashboard_title">
+                                            <h4>Classification</h4>
+                                        </section>
+                                    </section>
+                                    <section className="main-dashboard_info">
+                                        <h1>In Progress</h1>
+                                    </section>
+                                </section>
+                                <section className="col-lg-12 dashboard-rightSide_container">
+                                    <section className="main-dashboard_panel-blue">
+                                        <section className="main-dashboard_title">
+                                            <h4>Classification</h4>
+                                        </section>
+                                    </section>
+                                    <section className="main-dashboard_info">
+                                        <h1>In Progress</h1>
+                                    </section>
                                 </section>
                             </section>
                             <section className="dashboard-tabControl col-lg-9">
@@ -97,27 +122,30 @@ class Middle extends React.Component {
                                 </ul>
                             </section>
                             <section className="col-lg-9 dashboard-input">
-                                <input type="text" name="commentBox" ref="commentField" />
+                                <input type="text" name="commentBox" ref="commentField"/>
                                 <button onClick={
                                     () => {
 
-                                        (this.refs.commentField.value !== '') ? this.onSubmit(this.refs.commentField.value) : alert('Error');
+                                        (this.refs.commentField.value !== '') ? this.onSubmit(this.refs.commentField.value) : alert('Error: You can\'t add an empty string!');
                                         this.refs.commentField.value = '';
                                     }
                                 }>
-                                    Add Comment</button>
+                                    Add Comment
+                                </button>
                             </section>
                             <section className="col-lg-9 dashboard-commentBoard">
                                 <ul className="dashboard-commentBoard_list">
-                                {
-                                    (this.state.comments.length !== 0) ? this.state.comments.map((key, index) => {
-                                        return <li key={ index } className="new-item">{key}
-                                        <span style={{ float: 'right', color: 'black' }}>
-                                            <button onClick={() => { this.removeComment(index) }}>Delete</button>
+                                    {
+                                        (this.state.comments.length !== 0) ? this.state.comments.map((key, index) => {
+                                            return <li key={ index } className="new-item">{key}
+                                                <span style={{float: 'right', color: 'black'}}>
+                                            <button onClick={() => {
+                                                this.removeComment(index)
+                                            }}>Delete</button>
                                         </span>
-                                        </li>
-                                    }) : <h3>No comments! :(</h3>
-                                }
+                                            </li>
+                                        }) : <h3>No comments! :(</h3>
+                                    }
                                 </ul>
                             </section>
                         </section>
