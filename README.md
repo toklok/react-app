ed694ade9473d47a15945bd8a067f893728be402
Joseph Curtis <joecurt85@gmail.com>
2016-12-26 20:58:45 -0500

    final commit, fixed some issues, went through html css, started to refactor magic css numbers to variables

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 What does this show?
 ====================
 
-    1. Front end flow
-    2. Efficiently work under time constraints.
-    3. Basic AJAX GET request with promise based ajax lib and bind to DOM, user-profile.js, LOC 27, no authentication.
-    4. Structure is built on HTML 5.
-    5. Created condition mapping Array, middle.js, LOC 111.
-    6. Some animation visuals, the "search bar" with the SVG circle and the adding of the list item.
-    7. conditional binding if nothing is in the comment Array.
-    8. Used SVG as background in CSS
-    9. exceeds knowledge of front-end workflow.
-    10. Shows use of npm & git
-    11. Understands front-end tooling.
-    12. ES2015
+1. Front end flow
+2. Efficiently work under time constraints.
+3. Basic AJAX GET request with promise based ajax lib and bind to DOM, user-profile.js, LOC 27, no authentication.
+4. Structure is built on HTML 5.
+5. Created condition mapping Array, middle.js, LOC 111.
+6. Some animation visuals, the "search bar" with the SVG circle and the adding of the list item.
+7. conditional binding if nothing is in the comment Array.
+8. Used SVG as background in CSS
+9. exceeds knowledge of front-end workflow.
+10. Shows use of npm & git\
+11. Understands front-end tooling.
+12. ES2015
+
+How to run?
+===========
+
+1. Download repo
+2. npm install
+3. npm run-script dev-server for development, and it's only development at the moment.
--- src/components/middle.js
+++ src/components/middle.js
@@ -31,45 +31,44 @@ class Middle extends React.Component {
 
     render() {
         return (
-            <section className="col-lg-12 col-md-12 col-sm-12 main-dashboard">
-                <section className="col-lg-12 col-md-12 col-sm-12">
+            <section className="col-lg-12 main-dashboard">
+                <section className="col-lg-12">
                     <section className="panel panel-default">
                         <section className="main-panel_heading"><span className="main-dashboard_span">Opportunities</span> { `${this.props.title}` }</section>
                         <section className="panel-body">
-                           <section className="col-lg-8 col-md-8 col-sm-8">
+                           <section className="col-lg-9">
                                <span className="dashboard-icon"><i className="fa fa-user" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-2">
-                                   <div className="col-lg-12 text-center widget-user_title"><h6>assigned to:</h6></div>
-                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].assignee }</h6></div>
+                               <section className="widget-user col-lg-3">
+                                   <div className="widget-user_title"><h4>assigned to:</h4></div>
+                                   <div className="widget-user_data"><h3>{ this.dataSet[0].assignee }</h3></div>
                                </section>
                                <span className="dashboard-icon"><i className="fa fa-calendar" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-2">
-                                   <div className="col-lg-12 text-center widget-user_title"><h6>due date:</h6></div>
-                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].dateDue }</h6></div>
+                               <section className="widget-user col-lg-3">
+                                   <div className="widget-user_title"><h4>due date:</h4></div>
+                                   <div className="widget-user_data"><h3>{ this.dataSet[0].dateDue }</h3></div>
                                </section>
                                <span className="dashboard-icon"><i className="fa fa-exclamation-triangle" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-2">
-                                   <div className="col-lg-12 text-center widget-user_title"><h6>risk factor:</h6></div>
-                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].risk }</h6></div>
+                               <section className="widget-user col-lg-3">
+                                   <div className="widget-user_title"><h4>risk factor:</h4></div>
+                                   <div className="widget-user_data"><h3>{ this.dataSet[0].risk }</h3></div>
                                </section>
                                <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"/></span>
-                               <section className="widget-user col-lg-2">
-                                   <div className="col-lg-12 text-center widget-user_title"><h6>values:</h6></div>
-                                   <div className="col-md-12 text-center widget-user_data"><h6>{ this.dataSet[0].value }</h6></div>
+                               <section className="widget-user col-lg-3">
+                                   <div className="widget-user_title"><h4>values:</h4></div>
+                                   <div className="widget-user_data"><h3>{ this.dataSet[0].value }</h3></div>
                                </section>
                                <section className="dashboard-entity">
                                    <span className="dashboard-icon"><i className="fa fa-usd" aria-hidden="true"/></span>
-                                   <section className="widget-user col-lg-2">
-                                       <div className="col-lg-12 text-left widget-user_title"><h6>entity:</h6></div>
-                                       <div className="col-md-12 text-left widget-user_data"><h6>{ this.dataSet[0].entity }</h6></div>
+                                   <section className="widget-user col-lg-3">
+                                       <div className="widget-user_title"><h4>entity:</h4></div>
+                                       <div className="widget-user_data"><h3>{ this.dataSet[0].entity }</h3></div>
                                    </section>
                                </section>
                                     <section className="dashboard-notifications_alert">
                                         <p>Store <a href="#">Boston(130)</a> was found with High Damage Registration for High Risk Products in the last 7 days compare to previous 14 days and compare to North region bench mark activity.</p>
-                                        <div>Close</div>
                                     </section>
                                </section>
-                            <section className="col-lg-4 col-md-4 col-sm-4 dashboard-rightSide">
+                            <section className="col-lg-3 dashboard-rightSide">
                                 <section className="main-dashboard_panel">
                                     <section className="main-dashboard_title">
                                         <h4>Status</h4>
@@ -79,7 +78,7 @@ class Middle extends React.Component {
                                     <h1>In Progress</h1>
                                 </section>
                             </section>
-                            <section className="col-lg-4 col-md-4 col-sm-4 dashboard-rightSide">
+                            <section className="col-lg-3 dashboard-rightSide">
                                 <section className="main-dashboard_panel">
                                     <section className="main-dashboard_title">
                                         <h4>Status</h4>
@@ -89,7 +88,7 @@ class Middle extends React.Component {
                                     <h1>In Progress</h1>
                                 </section>
                             </section>
-                            <section className="dashboard-tabControl col-lg-8 col-md-8 col-sm-8">
+                            <section className="dashboard-tabControl col-lg-9">
                                 <ul>
                                     <li>Caught Items(4)</li>
                                     <li>Best Practices(3)</li>
@@ -97,7 +96,7 @@ class Middle extends React.Component {
                                     <li>History</li>
                                 </ul>
                             </section>
-                            <section className="col-lg-8 col-md-8 col-sm-8 dashboard-input">
+                            <section className="col-lg-9 dashboard-input">
                                 <input type="text" name="commentBox" ref="commentField" />
                                 <button onClick={
                                     () => {
@@ -108,8 +107,8 @@ class Middle extends React.Component {
                                 }>
                                     Add Comment</button>
                             </section>
-                            <section className="col-lg-8 col-md-8 col-sm-8 dashboard-commentBoard">
-                                <ul>
+                            <section className="col-lg-9 dashboard-commentBoard">
+                                <ul className="dashboard-commentBoard_list">
                                 {
                                     (this.state.comments.length !== 0) ? this.state.comments.map((key, index) => {
                                         return <li key={ index } className="new-item">{key}
